415ca9b8327d3d437457df88f469d5d72c491158
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-06-02 17:33:07 +0200

    update deployment

--- deploy/02-meme-nft-winner.js
+++ deploy/02-meme-nft-winner.js
@@ -1,14 +1,16 @@
 const {network} = require("hardhat")
 
+const openNftAddress = "0x37016d5175817F4a17A0eE27Fd6B448AEE4c55A9"
+
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
     let memeNFT;
     log("Deploying MemeNFTWinner")
     if (network.name === "localhost" || network.name === "matic" || network.name === "boba" || network.name === "mumbai") {
-        memeNFT = await deploy("MemeNFTWinner", {from: deployer, log: true, args: []})
+        memeNFT = await deploy("MemeNFTWinner", {from: deployer, log: true, args: [openNftAddress]})
     } else {
         throw new Error("Cannot deploy MemeNFTWinner - unsupported network")
     }
-    log("npx hardhat verify --network " + network.name + " " + memeNFT.address)
+    log("npx hardhat verify --network " + network.name + " " + memeNFT.address + " " + openNftAddress)
 }
